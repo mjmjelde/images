de0a465e570b64bfbccad844595a5de392cf862e
Matthew (mjmfighter) <mjmjelde@gmail.com>
2018-12-16 14:51:26 -0600

    Updated entrypoint to log to file and terminal

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,4 +22,4 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval ${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP} |& tee latest.log
